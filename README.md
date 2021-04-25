71d4127333aa434d8570219828fa29081aff49cd
Dastan21 <loicd21@gmail.com>
2021-04-25 15:17:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# VideoReupload <img src="icon.png" alt="Bot logo" width="32" style="border-radius: 50%;">
+
+Reuploads Twitter videos from link to Discord.
+
+## Invite link
+https://discord.com/api/oauth2/authorize?client_id=835849123751133185&permissions=40960&scope=bot
---
+++ bot.js
@@ -0,0 +1,28 @@
+const videoUrlLink = require('video-url-link');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config');
+
+bot.on('ready', () => { console.log(bot.user.tag + " is online"); });
+
+bot.on('message', msg => {
+	if (!msg.guild) return;
+	if (!msg.channel.name.includes("meme")) return;
+	if (!msg.content.includes("https://twitter.com") && !msg.content.includes("https://t.co")) return;
+	downloadVideo(msg);
+});
+
+function downloadVideo(msg) {
+	const args = msg.content.split(' ');
+	let url = null;
+	for (const a of args) if (a.startsWith("https://twitter.com") || a.startsWith("https://t.co")) { url = a; break; }
+	if (!url) return;
+	videoUrlLink.twitter.getInfo(url, {}, (error, info) => {
+		if (error) return console.error(error);
+		const video = info.variants.sort((a, b) => b.bitrate - a.bitrate)[0];
+		msg.channel.send(video.url).catch(()=>{});
+	});
+	msg.delete().catch(()=>{});
+}
+
+bot.login(config.token);
