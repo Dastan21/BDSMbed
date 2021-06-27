7b9e39750c1062cfe0754d0059ba3289d2acde73
Dastan21 <loicd21@gmail.com>
2021-06-27 22:29:17 +0200

    fixed facebook

--- modules/facebook.js
+++ modules/facebook.js
@@ -4,8 +4,9 @@ const links = ["https://www.facebook.com", "https://m.facebook.com"];
 
 function getVideo(url, size_limit) {
 	return new Promise((resolve, reject) => {
+		if (url.endsWith("/")) url = url.slice(0, -1);
 		fbdl.getInfo(url).then(res => {
-			if (!res.rawVideo) return reject();
+			if (!res?.rawVideo) return reject();
 			axios.get(res.rawVideo, { responseType: 'arraybuffer' }).then(video_data => {
 				if (video_data.data.length > size_limit) return reject();
 				resolve({ upload: true, data: video_data.data, name: (res.name || "facebook").replace(/\s/gm, "") + ".mp4" });
